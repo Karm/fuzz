e2fcb20da1e75c3d108a26142e7afee0d0fff855
Michal Karm Babacek <karm@redhat.com>
2023-09-19 12:34:38 +0200

    Depends on https://karms.biz/tmp/diff.patch.txt

--- src/test/java/biz/karms/fuzz/RESTTestIT.java
+++ src/test/java/biz/karms/fuzz/RESTTestIT.java
@@ -1,24 +1,7 @@
 package biz.karms.fuzz;
 
-import io.quarkus.runtime.ApplicationLifecycleManager;
-import io.quarkus.runtime.Quarkus;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.junit.jupiter.api.AfterAll;
 
-import java.util.logging.Logger;
-
-@QuarkusIntegrationTest
+@QuarkusIntegrationTest(destroyProcessInAfterAll = true)
 public class RESTTestIT extends RESTTest {
-
-    /* Did not help
-    private static final Logger LOG = Logger.getLogger(RESTTestIT.class.getName());
-
-    @AfterAll
-    public static void teardown() throws InterruptedException {
-        LOG.info("Stopping Quarkus");
-        ApplicationLifecycleManager.exit(-1);
-        Thread.sleep(1000);
-    }
-*/
-
 }
